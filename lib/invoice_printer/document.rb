0983c087640d7198033d1eda13bb5e075ae23650
Josef Strzibny <strzibny@strzibny.name>
2016-08-02 23:31:58 +0200

    Support notes
    Fixes #2

--- lib/invoice_printer/document.rb
+++ lib/invoice_printer/document.rb
@@ -34,7 +34,8 @@ module InvoicePrinter
   #     items: [
   #       InvoicePrinter::Document::Item.new,
   #       InvoicePrinter::Document::Item.new
-  #     ]
+  #     ],
+  #     note: 'A note at the end.'
   #   )
   #
   # +amount should equal the sum of all item's +amount+,
@@ -77,7 +78,8 @@ module InvoicePrinter
                 :account_iban,
                 :account_swift,
                 # Collection of InvoicePrinter::Invoice::Items
-                :items
+                :items,
+                :note
 
     def initialize(number: nil,
                    provider_name: nil,
@@ -108,7 +110,8 @@ module InvoicePrinter
                    bank_account_number: nil,
                    account_iban: nil,
                    account_swift: nil,
-                   items: nil)
+                   items: nil,
+                   note: nil)
       @number = String(number)
       @provider_name = String(provider_name)
       @provider_ic = String(provider_ic)
@@ -139,9 +142,50 @@ module InvoicePrinter
       @account_iban = String(account_iban)
       @account_swift = String(account_swift)
       @items = items
+      @note = String(note)
 
       raise InvalidInput, 'items are not only a type of InvoicePrinter::Document::Item' \
         unless @items.select{ |i| !i.is_a?(InvoicePrinter::Document::Item) }.empty?
+    end
+
+    def to_h
+      {
+        'number': @number,
+        'provider_name': @provider_name,
+        'provider_ic': @provider_ic,
+        'provider_dic': @provider_dic,
+        'provider_street': @provider_street,
+        'provider_street_number': @provider_street_number,
+        'provider_postcode': @provider_postcode,
+        'provider_city': @provider_city,
+        'provider_city_part': @provider_city_part,
+        'provider_extra_address_line': @provider_extra_address_line,
+        'purchaser_name': @purchaser_name,
+        'purchaser_ic': @purchaser_ic,
+        'purchaser_dic': @purchaser_dic,
+        'purchaser_street': @purchaser_street,
+        'purchaser_street_number': @purchaser_street_number,
+        'purchaser_postcode': @purchaser_postcode,
+        'purchaser_city': @purchaser_city,
+        'purchaser_city_part': @purchaser_city_part,
+        'purchaser_extra_address_line': @purchaser_extra_address_line,
+        'issue_date': @issue_date,
+        'due_date': @due_date,
+        'subtotal': @subtotal,
+        'tax': @tax,
+        'tax2': @tax2,
+        'tax3': @tax3,
+        'total': @total,
+        'bank_account_number': @bank_account_number,
+        'account_iban': @account_iban,
+        'account_swift': @account_swift,
+        'items': @items.map(&:to_h),
+        'note': @note
+      }
+    end
+
+    def to_json
+      to_h.to_json
     end
   end
 end
